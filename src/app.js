6702585740a7aa064f7203b3d9205b07a51b20ea
22devkpatel <22devkpatel@gmail.com>
2025-03-12 22:31:23 +0000

    some message

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Arrays containing word categories
+  let pronoun = ['pro', 'epic', 'shadow'];
+  let adj = ['fast', 'stealth', 'legendary'];
+  let noun = ['gamer', 'warrior', 'arena', 'clan', 'quest'];
+
+  // Generating domain names
+  let domains = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        domains.push(pronoun[i] + adj[j] + noun[k] + '.com');
+      }
+    }
+  }
+
+  document.getElementById("domain-list").innerHTML = domains
+  .map(domain => `<li>${domain}</li>`)
+  .join("");
 };
+//   let domainList = document.getElementById("domain-list");
+
+//   domainList.innerHTML = ""; 
+//   domains.forEach(domain => {
+//     let listItem = document.createElement("li"); 
+//     listItem.textContent = domain; 
+//     domainList.appendChild(listItem); 
+//   });
+// };
